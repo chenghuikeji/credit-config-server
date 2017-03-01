9e0bd25ab222a94237f4f6b5da6ca6fe47e2a77e
mars <dianqi.yu@hotmail.com>
2017-03-01 16:26:07 +0800

    b

--- config-server2/src/main/java/com/chenghui/ConfigServer2Application.java
+++ config-server2/src/main/java/com/chenghui/ConfigServer2Application.java
@@ -2,7 +2,9 @@ package com.chenghui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer //添加@EnableConfigServer注解，开启Config Server
 @SpringBootApplication
 public class ConfigServer2Application {
 
