7c6258e64ea71eda3e493ad6b6c0363604624564
mars <dianqi.yu@hotmail.com>
2017-03-03 10:41:19 +0800

    complete

--- config-server2/src/main/java/com/chenghui/ConfigServer2Application.java
+++ config-server2/src/main/java/com/chenghui/ConfigServer2Application.java
@@ -2,8 +2,10 @@ package com.chenghui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableDiscoveryClient //新增@EnableDiscoveryClient注解，用来将config-server注册到配置的服务注册中心上去
 @EnableConfigServer //添加@EnableConfigServer注解，开启Config Server
 @SpringBootApplication
 public class ConfigServer2Application {
